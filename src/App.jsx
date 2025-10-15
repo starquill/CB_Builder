9ed00686a16a1197cff161da3e75112d0c85365a
starquill <ajay.2201075cs@iiitbh.ac.in>
2025-10-15 11:01:10 +0530

    Add controlled input for name field

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import React,{useState} from "react";
 
 function GeneralInfo(){
   const [name,setName]=useState("john Doe");
+  function handleNameChange(e){
+    setName(e.target.value);
+  }
   return(
   <div>
   <h2>General Info</h2>
   <p>Name:{name}</p>
+  <input type="text"
+      value={name}
+      onChange={handleNameChange} />
   </div>
   )
 }
