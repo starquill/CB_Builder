a95fe02d655a7af30918ae708af7a3f9dab84b00
starquill <ajay.2201075cs@iiitbh.ac.in>
2025-10-15 19:27:14 +0530

    Education: Add form to add new education entries

--- src/App.jsx
+++ src/App.jsx
@@ -74,7 +74,27 @@ function Education(){
       degree: 'M.S. in Software Engineering',
       date: '2026 - 2028',
     },
-  ])
+  ]);
+  const [newEducation,setNewEdcation]=useState({
+    school:'',
+    degree:'',
+    date:'',
+  });
+  function handleChange(e){
+    const {name,value}=e.target;
+    setNewEdcation(prevEducation=>({
+      ...prevEducation,
+      [name]:value,
+    }));
+  }
+  function handleAdd(e){
+    e.preventDefault();
+    setEducations([
+      ...educations,
+      {...newEducation,id:crypto.randomUUID()},
+    ]);
+    setNewEdcation({school:'',degree:'',date:''});
+  }
   return (<div>
     <h2>Education</h2>
     {educations.map(education => (
@@ -83,6 +103,32 @@ function Education(){
       <p><strong>Degree:</strong> {education.degree}</p>
       <p><strong>Date:</strong> {education.date}</p>
     </div>))}
+    <hr />
+    <form onSubmit={handleAdd}>
+      <label htmlFor="school">School:</label>
+      <input type="text"
+      id="school"
+      name="school"
+      value={newEducation.school}
+      onChange={handleChange}
+      />
+      <label htmlFor="degree">Degree:</label>
+        <input
+          type="text"
+          id="degree"
+          name="degree"
+          value={newEducation.degree}
+          onChange={handleChange}
+        />
+      <label htmlFor="date">Date:</label>
+      <input type="text"
+      id="date"
+      name="date"
+      value={newEducation.date}
+      onChange={handleChange}
+      />
+      <button type="submit">Add Education</button>
+    </form>
   </div>);
 }
 export default function App(){
