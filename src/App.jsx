c1bcd3715e5894392a8f2bded9b81eec2e105e31
starquill <ajay.2201075cs@iiitbh.ac.in>
2025-10-15 11:44:17 +0530

    Manage all general info fields in a single state object

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
 import React,{useState} from "react";
 
 function GeneralInfo(){
-  const [name,setName]=useState("john Doe");
+  const [info,setInfo]=useState({
+    name:"John doe",
+    email:"johndoe@official.com",
+    phone:"123-456-789",
+  });
   function handleNameChange(e){
-    setName(e.target.value);
+    const {value,name}=e.target;
+    setInfo(prevInfo=>({
+      ...prevInfo,
+      [name]:value,
+    }));
   }
   return(
   <div>
   <h2>General Info</h2>
   <p>Name:{name}</p>
+  <label htmlFor="name">Name:</label>
   <input type="text"
-      value={name}
+      id="name"
+      name="name"
+      value={info.name}
       onChange={handleNameChange} />
+    <label htmlFor="email">Email:</label>
+   <input type="text"
+      id="email"
+      name="email"
+      value={info.email}
+      onChange={handleNameChange}
+      />
+    <label htmlFor="phone">Phone:</label>
+    <input type="tel"
+    id="phone"
+    name="phone"
+    value={info.phone}
+    onChange={handleNameChange}
+     />
+     <pre>
+        <code>{JSON.stringify(info, null, 2)}</code>
+      </pre>
   </div>
+  
+  
   )
 }
 export default function App(){
